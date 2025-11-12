f3a9ee77a5797cde115bf7f03893dbfb21d8ed04
ncarmona99 <nicolascarmonarioseco@gmail.com>
2025-11-11 22:47:21 -0300

    Tailwind classes added

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,26 +1,31 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>
-    <header>
-      <nav class="flex flex-row bg-sky-500 text-white p-4 space-x-4">
+  <header>
+    <div class="flex flex-row justify-center">
+      <nav
+        class="sticky top-0 mx-auto items-center justify-center flex flex-row my-5 rounded-xl bg-slate-300 text-white text-2xl p-6 gap-x-4"
+      >
         <NuxtLink
           to="/"
-          class="flex flex-row rounded-xl bg-sky-300 text-white p-4 space-x-4 transition-all hover:bg-sky-700"
+          class="relative inline-block px-1 after:content-[''] after:absolute after:left-0 after:bottom-0 after:h-[2px] after:w-0 after:bg-white after:transition-all after:duration-300 hover:after:w-full"
           >Home</NuxtLink
         >
+        <div class="w-px h-9 bg-white opacity-70"></div>
         <NuxtLink
           to="/projects"
-          class="flex flex-row rounded-xl bg-sky-300 text-white p-4 space-x-4 transition-all hover:bg-sky-700"
+          class="relative inline-block px-1 after:content-[''] after:absolute after:left-0 after:bottom-0 after:h-[2px] after:w-0 after:bg-white after:transition-all after:duration-300 hover:after:w-full"
           >Projects</NuxtLink
         >
       </nav>
-    </header>
+    </div>
+  </header>
 
-    <main>
-      <slot />
-    </main>
+  <main>
+    <slot />
+  </main>
 
-    <footer>© {{ new Date().getFullYear() }} Nicolás Carmona</footer>
-  </div>
+  <footer>
+    <div>© {{ new Date().getFullYear() }} Nicolás Carmona</div>
+  </footer>
 </template>
