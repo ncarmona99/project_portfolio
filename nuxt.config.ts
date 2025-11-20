4ff70015dd755405b85828ee25994b00e60411c4
ncarmona99 <n.carmona@duocuc.cl>
2025-11-20 09:12:00 -0300

    added to nuxt.config.ts a directive to assign classes for fade effect going from one page to another

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,18 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: false },
   srcDir: "app",
+  app: {
+    head: {
+      titleTemplate: "%s - Nicolás Carmona portfolio",
+      meta: [
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+      ],
+    },
+    pageTransition: {
+      name: "page",
+      mode: "out-in",
+    },
+  },
 
   css: ["~/assets/css/main.css"],
 
